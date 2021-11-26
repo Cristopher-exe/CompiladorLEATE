0892cecefa5ea033d8b8f30962054efa9fb48b09
martinhoyos <91520138+martinhoyos@users.noreply.github.com>
2021-11-26 01:06:27 -0700

    Actualización llenado de la tabla

--- src/Frontend/VentanaIdentificadores.java
+++ src/Frontend/VentanaIdentificadores.java
@@ -53,16 +53,9 @@ public class VentanaIdentificadores extends javax.swing.JFrame {
                 "Identificador", "Tipo", "Valor", "Linea"
             }
         ) {
-            Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class
-            };
             boolean[] canEdit = new boolean [] {
                 false, false, true, false
             };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
